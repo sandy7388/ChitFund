53d0ff8d529f9eb56cd7743f3e0135bc85bbfd80
sandy <sandy.kshirsagar0@gmail.com>
2018-03-24 17:55:08 +0530

    Thanks

--- app/src/main/java/chitfund/wayzontech/chitfund/chitfund/fragment/EditProfileFragment.java
+++ app/src/main/java/chitfund/wayzontech/chitfund/chitfund/fragment/EditProfileFragment.java
@@ -23,7 +23,8 @@ public class EditProfileFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         View view = inflater.inflate(R.layout.fragment_edit_profile,
                 container, false);
         return view;
