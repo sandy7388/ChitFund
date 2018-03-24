3a722b5efa2b24f18d6827c769d4faf42255c574
sandy <sandy.kshirsagar0@gmail.com>
2018-03-24 17:46:02 +0530

    Thanks

--- app/src/main/java/chitfund/wayzontech/chitfund/chitfund/fragment/EditProfileFragment.java
+++ app/src/main/java/chitfund/wayzontech/chitfund/chitfund/fragment/EditProfileFragment.java
@@ -2,11 +2,15 @@ package chitfund.wayzontech.chitfund.chitfund.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import chitfund.wayzontech.chitfund.chitfund.R;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by sandy on 24/3/18.
@@ -23,5 +27,21 @@ public class EditProfileFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_edit_profile,
                 container, false);
         return view;
+    }
+
+    public void setFragments(Fragment targetFragment) {
+        try {
+            Fragment fragment = targetFragment;
+            if (fragment != null) {
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.content, fragment).commit();
+            } else {
+                // error in creating fragment
+                Log.e(TAG, "Error in creating fragment");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
